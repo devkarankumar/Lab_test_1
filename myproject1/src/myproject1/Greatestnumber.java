8e71d46d9feae99bd14ecb469e79df52907ecd9e
Devkaran Saw <Devkaran Saw@DESKTOP-5EDRU9S>
2022-12-16 12:53:12 +0530

    labwork

---
+++ myproject1/src/myproject1/Greatestnumber.java
@@ -0,0 +1,36 @@
+
+package myproject1;
+import java.util.Scanner;
+public class Greatestnumber {
+
+	public static void main(String[] args) {
+int x, y, z;
+        
+        
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the first number:");
+        x = s.nextInt();
+        System.out.print("Enter the second number:");
+        y = s.nextInt();
+        System.out.print("Enter the third number:");
+        z = s.nextInt();
+        
+        if(x > y && x > z)
+        {
+            System.out.println("The largest number is: "+x);
+        }
+        else if(y > z)
+        {
+            System.out.println("The largest number is: "+y);
+        }
+        else
+        {
+            System.out.println("The largest number is: "+z);
+        }
+ 
+    }
+
+
+	}
+
+
---
+++ myproject1/src/myproject1/Operation.java
@@ -0,0 +1,37 @@
+package myproject1;
+import java.util.Scanner;
+public class Operation {
+
+	public static void main(String[] args) {
+		int num1,num2;
+        double output=0;
+	char operator;
+        Scanner sc=new Scanner(System.in);
+	System.out.println("Enter a Binary Expression:");
+	num1=sc.nextInt();
+     	operator=sc.next().charAt(0);
+      num2=sc.nextInt();
+        switch(operator)
+	{
+		case '+':
+			output=num1+num2;
+			break;
+		case '-':
+			output=num1-num2;
+			break;
+		case '*':
+			output=num1*num2;
+			break;
+		case '/':
+			output=num1/num2;
+			break;
+		case '%':
+			output=num1%num2;
+			break;
+		default :
+			System.out.println("invalid operation");
+			break;
+	}
+	System.out.println("The Output Is:"+output);
+   }
+}
