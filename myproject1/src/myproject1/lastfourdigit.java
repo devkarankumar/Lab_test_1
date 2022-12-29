0ecf3d1cc8194ba13c375e6afd6ba3ed5b468604
Devkaran Saw <Devkaran Saw@DESKTOP-5EDRU9S>
2022-12-29 21:29:17 +0530

    labtest4

---
+++ myproject1/src/myproject1/lastfourdigit.java
@@ -0,0 +1,15 @@
+package myproject1;
+
+public class lastfourdigit {
+
+	public static void main(String[] args) {
+		long f =93156285;
+	    System.out.println(
+	      Long.toString(f).substring(
+	       Long.toString(f).length() - 4)); 
+	    
+	  }
+	
+	}
+
+
